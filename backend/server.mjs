e84b437b51a814a9ce636fcaa18a6961a1d5f0cc
ALESSANDRO GENCO <s324502@studenti.polito.it>
2024-10-05 16:23:04 +0200

    debugging server

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,53 +1,21 @@
 import ServiceDao from './dao/service.mjs';
+import cors from'cors';
+import express from 'express';
 
-const cors = require('cors');
-const express = require('express');
-//const path = require('path');
 const app = express();
+const serviceDao = new ServiceDao();
+const PORT = 3000;
+
 app.use(express.json());
 
-const serviceDao = new ServiceDao();
-
-/*
-// Server React Project 1
-app.use('/project1', express.static(path.join(__dirname, 'frontend/project1/build')));
-
-// Server React Project 2
-app.use('/project2', express.static(path.join(__dirname, 'frontend/project2/build')));
-
-// Server React Project 3
-app.use('/project3', express.static(path.join(__dirname, 'frontend/project3/build')));
-
-// Fallback to index.html for React Router
-app.get('*', (req, res) => {
-  if (req.path.startsWith('/project1')) {
-    res.sendFile(path.join(__dirname, 'project1/build', 'index.html'));
-  } else if (req.path.startsWith('/project2')) {
-    res.sendFile(path.join(__dirname, 'project2/build', 'index.html'));
-  } else if (req.path.startsWith('/project3')) {
-    res.sendFile(path.join(__dirname, 'project3/build', 'index.html'));
-  } else {
-    res.status(404).send('Not Found');
-  }
-});
-*/
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 const corsOptions = {
-  origin: [`http://localhost:${PORT}`], // Aggiungi tutte le origini autorizzate
-  credentials: true // Permetti l'invio di cookie nelle richieste CORS
+  origin: 'http://localhost:${PORT}',
+  optionsSuccessStatus: 200,
+  credentials: true
 };
-
 app.use(cors(corsOptions));
-initRoutes(app)
-if (!module.parent) {
-  app.listen(PORT, () => {
-      console.log(`Server listening at http://localhost:${PORT}`);
-  });
-}
+
+/* API */
 
 /* Get ticket API */
 app.get('/api/newTicket', async(req, res) => {
@@ -60,4 +28,9 @@ app.get('/api/newTicket', async(req, res) => {
   }catch(err){
     res.status(500).end();
   }
+});
+
+/* ACTIVATING THE SERVER */
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
