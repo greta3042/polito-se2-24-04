afea8e45885f7b9f6f23b9bcb238b00f11f9ffe7
ALESSANDRO GENCO <s324502@studenti.polito.it>
2024-10-05 18:28:10 +0200

    minor changes to file names

--- backend/test_unit/dao_tests/service.test.js
+++ backend/test_unit/dao_tests/service.test.js
@@ -1,4 +1,4 @@
-import { describe, test, expect, jest, afterEach } from "@jest/globals"
+import { describe, test, expect, jest, afterEach } from "@jest/globals";
 import db from "../db/db.mjs"
 
 // import the dao
