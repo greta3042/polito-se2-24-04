48ccd03981c526b5a0ddaaaf058a2abc0974562a
Greta Tumiatti <s332148@studenti.polito.it>
2024-10-07 09:03:11 +0200

    small changes front end counter

--- frontend/counter/src/Login.js
+++ frontend/counter/src/Login.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aggiungi qui la logica di autenticazione
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aggiungi qui la logica di autenticazione se necessario
+    navigate('/homepage'); // Naviga alla pagina homepage
   };
 
   return (
@@ -17,24 +17,19 @@ function Login() {
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="username">Counter:</label>
-          <input
-            type="text"
-            id="username"
+          <label htmlFor="counter">Counter:</label>
+          <select
+            id="counter"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          >
+            <option value="c1">c1</option>
+            <option value="c2">c2</option>
+            <option value="c3">c3</option>
+            <option value="c4">c4</option>
+            <option value="c5">c5</option>
+          </select>
         </div>
         <button type="submit">Login</button>
       </form>
