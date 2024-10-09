cb25f05630fed674b3d712b6a8b54e2b47f7a96a
Greta Tumiatti <s332148@studenti.polito.it>
2024-10-09 10:52:04 +0200

    add button totem and timer

--- frontend/totem/src/QRCodePage.js
+++ frontend/totem/src/QRCodePage.js
@@ -1,17 +1,28 @@
 import './QRCodePage.css';
-import { useLocation } from 'react-router-dom';
-import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { QRCodeCanvas } from 'qrcode.react'; // QRCodeCanvas to generate QR code
 import Button from 'react-bootstrap/Button'; // Button import
 
 function QRCodePage() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [ticket, setTicket] = useState('10 servizio');
   const { code, serviceName } = location.state;
-  const [ticket, setTicket] = useState('10 servizio'); // State for the ticket number
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/chooseservice');
+    }, 60000); // 60000 milliseconds = 1 minute
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, [navigate]);
+
   
-    const removeTicket = () => {
-      setTicket('0');
-    }
+  const handleBackClick = () => {
+    navigate('/chooseservice');
+  };
+
   
     return (
         <div className="centered-container">
@@ -23,7 +34,7 @@ function QRCodePage() {
             <div>
               <QRCodeCanvas className='qr_code' value={ticket} size={200}/>
               <br/>
-              <Button className='new_ticket' variant="primary" onClick={removeTicket}>Get a new ticket</Button> 
+              <Button className='new_ticket' variant="primary" onClick={handleBackClick}>Go back to services</Button> 
             </div>
           }
         </div>
