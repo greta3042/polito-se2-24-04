d003ce767f0434da2f646487a00c42508ab0646b
ALESSANDRO GENCO <s324502@studenti.polito.it>
2024-10-16 22:59:47 +0200

    Updated display

--- frontend/display/src/App.js
+++ frontend/display/src/App.js
@@ -1,70 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import io from 'socket.io-client';
-import { useEffect } from 'react';
-
 
 function App() {
-  const [notifications, setNotifications] = useState([]);  
+  const [notifications, setNotifications] = useState([]);
 
-  // Fill notifications at the start with all the counters
+  // Fetch counters, used only at first rendering
   const fetchCounters = async () => {
     try {
       const response = await fetch('http://localhost:3001/api/counters');
       const data = await response.json();
-      setNotifications(data.map(counterId => {
-        return { counterId: String(counterId.id), service: '', customerNumber: 'No customer being served' };
-      }));
+      setNotifications(data.map(counterId => ({
+        counterId: String(counterId.id),
+        service: '',
+        customerNumber: 'No customer being served'
+      })));
     } catch (error) {
       console.error('Error fetching counters:', error);
     }
   };
 
-  useEffect(() => {
+  // Update notifications
+  const handleUpdate = (data) => {
+    setNotifications(notifications => 
+      notifications.map(notification => 
+        notification.counterId === data.counterId
+          ? data            // Update the counter with the new customer served
+          : notification    // Keep unchanged notifications
+      )
+    );
+  };
 
-    // Call fetchCounters to load initial data
+  useEffect(() => {
     fetchCounters();
 
-    const socket = io('http://localhost:4001'); // Backend server URL
+    // Connect to the WebSocket
+    const socket = io('http://localhost:4001'); // Backend URL
 
-    // Callback called when a notification is received
+    // Handle incoming notification from WebSocket
     socket.on('nextCustomer', (data) => {
-      console.log(notifications);
-      console.log(data);
-      // Update the state with information to display for counters
-      setNotifications(notifications.map(notification =>
-        notification.counterId === data.counterId
-          ? data            // Replace the entry with the same counterId
-          : notification)   // Keep the other entries unchanged
-        );
+      handleUpdate(data); // Update notifications 
     });
 
-    // Cleanup to disconnect component at component life end
+    // Clean up on component unmount (disconnect socket)
     return () => {
-        socket.disconnect();
+      socket.disconnect();
     };
-}, []); // Empty array means that the socket is opened only at the starting at the component and keep listening
+  }, []); // Empty dependency array makes this running only at the first rendering
 
   return (
     <div className="App">
-        <table className="display_table" border="1">
-          <thead>
-            <tr className="first_row">
-              <th>Counter</th>
-              <th>Service</th>
-              <th>Customer</th>
+      <table className="display_table" border="1">
+        <thead>
+          <tr className="first_row">
+            <th>Counter</th>
+            <th>Service</th>
+            <th>Customer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notifications.map((notification, index) => (
+            <tr key={index}>
+              <td>{notification.counterId}</td>
+              <td>{notification.service}</td>
+              <td>{notification.customerNumber}</td>
             </tr>
-          </thead>
-          <tbody>
-            {notifications.map((notification, index) => (
-              <tr key={index}>
-                <td>{notification.counterId}</td>
-                <td>{notification.service}</td>
-                <td>{notification.customerNumber}</td>
-              </tr>
           ))}
-          </tbody>
-        </table>
+        </tbody>
+      </table>
     </div>
   );
 }
