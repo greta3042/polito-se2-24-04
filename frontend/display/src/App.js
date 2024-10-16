f022db27755275a14b637bee83cf8583dfbb2ac0
ALESSANDRO GENCO <s324502@studenti.polito.it>
2024-10-16 20:21:42 +0200

    updated frontend for display

--- frontend/display/src/App.js
+++ frontend/display/src/App.js
@@ -15,11 +15,13 @@ function App() {
 
     // Callback called when a notification is received
     socket.on('nextCustomer', (data) => {
-        console.log(`Notify received: Next customer ${data.customerNumber} service: ${data.service} counter: ${data.counterId}`);
-        const { customerNumber, service, counterId } = data;
-        setNotifications((prevNotifications) => [...prevNotifications, {customerNumber, service, counterId}]);
-        // Update the state with information to display for counters
-        // to do
+
+      // Update the state with information to display for counters
+      setNotifications(notifications.map(notification =>
+        notification.counterId === data.counterId
+          ? data  // Replace the entry with the same counterId
+          : notification) // Keep the other entries unchanged
+        )  
     });
 
     // Cleanup to disconnect component at component life end
@@ -27,6 +29,8 @@ function App() {
         socket.disconnect();
     };
 }); // Empty array means that the socket is opened only at the starting at the component and keep listening
+
+
 
   return (
     <div className="App">
