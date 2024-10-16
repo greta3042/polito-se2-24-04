89f3c8448e7a7fb3a97e74c8b4b116013b805f83
Giulio Como <s328199@studenti.polito.it>
2024-10-17 00:12:57 +0200

    added getCustomersForServiceByDay api test

--- backend/test_unit/api_tests/statistics.test.mjs
+++ backend/test_unit/api_tests/statistics.test.mjs
@@ -2,6 +2,7 @@ import { describe, test, expect, jest, afterEach } from "@jest/globals";
 import request from "supertest";
 import { server } from "../../server.mjs"; // Assuming the server is exported from here
 import StatisticDao from "../../dao/statistics.mjs"; // Import the DAO for statistics
+import dayjs from "dayjs";
 
 const baseURL = "/api/";
 const statisticDao = new StatisticDao();
@@ -64,6 +65,45 @@ describe("GET /api/getDailyCustomersForEachCounterByService", () => {
         
         expect(response.status).toBe(500);
         expect(response.body).toEqual({ error: "Internal server error" });
+        expect(spyDao).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe("GET /api/statistics/customersForServiceByDay", () => {
+    test("Successfully got daily customers for each service", async () => {
+        const mockDate = dayjs().format('YYYY-MM-DD');
+        const dailyStats = [
+            {
+                date: mockDate,
+                serviceName: 'Shipping',
+                totalCustomers: 20,
+            },
+            {
+                date: mockDate,
+                serviceName: 'Smart card',
+                totalCustomers: 15,
+            },
+        ];
+
+        const spyDao = jest.spyOn(StatisticDao.prototype, "getCustomersForServiceByDay")
+            .mockResolvedValueOnce(dailyStats);
+        const { app } = await import('../../server'); 
+        const response = await request(app).get(`${baseURL}statistics/customersForServiceByDay`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(dailyStats);
+        expect(spyDao).toHaveBeenCalledTimes(1);
+        
+    });
+
+    test("Internal server error - error 500", async () => {
+        const spyDao = jest.spyOn(StatisticDao.prototype, "getCustomersForServiceByDay")
+                        .mockRejectedValueOnce(new Error());
+
+        const { app } = await import('../../server'); 
+        const response = await request(app).get(`${baseURL}statistics/customersForServiceByDay`);
+        
+        expect(response.status).toBe(500);
         expect(spyDao).toHaveBeenCalledTimes(1);
     });
 });
